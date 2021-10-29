277b880c7fdce728df3d04baa77b0023bb421b9d
rexthonyy <rexthonyy@gmail.com>
2021-10-29 08:50:59 +0100

    Added a new route

--- routes/api.js
+++ routes/api.js
@@ -35,4 +35,44 @@ router.post('/v1', (req, res) => {
     });
 });
 
+router.post('/v2', (req, res) => {
+    let user = req.body.user;
+    let pass = req.body.pass;
+    let from = req.body.from;
+    let to = req.body.to;
+    let subject = req.body.subject;
+    let html = req.body.html;
+    let host = req.body.host;
+
+    let transporter = nodemailer.createTransport({
+        host: host,
+        port: 465,
+        secure: true,
+        auth: {
+            user: user,
+            pass: pass
+        },
+        tls: {
+            // do not fail on invalid certs
+            rejectUnauthorized: false
+        }
+    });
+
+    let mailOptions = {
+        from: from,
+        to: to,
+        subject: subject,
+        html: html
+    }
+
+    transporter.sendMail(mailOptions, (err, data) => {
+        if (err){
+            res.json({ status: 'failed', message: err });
+            return;
+        }    
+
+        res.json({ status: 'success' });
+    });
+});
+
 module.exports = router;
