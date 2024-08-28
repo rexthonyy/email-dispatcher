7deefea889f539b59df0dfe5d06318aa801e05b8
rexthony <rexthonyy@gmail.com>
2024-08-28 19:19:33 +0100

    update

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const apiRouter = require('./routes/api');
-
+//UPDATE
 const app = express();
 
 app.use(express.json());
 app.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-	res.header("Access-Control-Allow-Headers", "Content-Type");
-	next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
 });
 app.use(express.static('public'));
 
